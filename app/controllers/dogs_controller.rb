508e2ffbf1c9f3d77aaf7b3907f33874b230ca69
Janae Hall <janaeelisa@gmail.com>
2019-11-07 17:54:15 -0600

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,19 @@
 class DogsController < ApplicationController
+  before_action :find_dog, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @owners = @dog.employees
+  end
+
+  private
+
+  def find_dog
+  @dog = Dog.find(params[:id])
+  end
+
 end
